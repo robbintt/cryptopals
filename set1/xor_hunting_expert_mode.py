d058255df9f2a682f6e402b36c4cb01b1601d984
Trent Robbins <robbintt@gmail.com>
2017-08-18 00:05:15 -0700

    Added 1.4

---
+++ set1/xor_hunting_expert_mode.py
@@ -0,0 +1,31 @@
+''' XOR hunt with a bigger dataset
+
+answer: [10231.933333333334, '5', 'Now that the party is jumping\n']
+
+Issue: python readlines() was giving me a '\n' making each string not ascii
+Response: I stripped the lines after importing them before processing them.
+'''
+from xor_hunting import *
+
+
+if __name__ == '__main__':
+    '''
+    '''
+
+    result_list = list()
+
+    with open("data/4.txt") as f:
+        for line in f.readlines():
+            line = line.strip() # strip the newline from readlines() function
+            for c in test_characters:
+                result = xor_by_char(line, c)
+                if not result:
+                    break
+
+                score = letter_frequency_scorer(result)
+                    
+                result_list.append([score, c, result])
+
+    result_list.sort(key=lambda x: x[0])
+    for each in result_list:
+        print each
