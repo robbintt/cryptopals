3e0784538b17968d72043dd9c49b1b0b349f0601
Trent Robbins <robbintt@gmail.com>
2017-08-18 02:07:51 -0700

    Minor edits to repeating key xor

--- set1/repeating_key_xor.py
+++ set1/repeating_key_xor.py
@@ -2,15 +2,6 @@
 
 ''' 
 import base64
-import logging
-
-logging.basicConfig(filename='error.log', level=logging.DEBUG)
-
-# turned them into characters even though we are using xor with ints
-test_characters = [chr(x) for x in range(256)]
-
-not_hex_inputs_filename = 'not-hex.log'
-
 
 def repeating_key_xor(target, keys):
     ''' use repeating key xor
@@ -34,12 +25,13 @@ if __name__ == '__main__':
 
     input1 = "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal"
 
-    # using a list here allows for multi-character keys
+    # also could use a string and split it later. Might make more sense for later problems.
     repeating_keys = ['I', 'C', 'E']
 
     result1 = base64.b16encode(repeating_key_xor(input1, repeating_keys))
 
-    print result1
     expected_result = "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"
+
+    # check result against expected_result
     print result1 == expected_result.upper()
 
