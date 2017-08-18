566041fa183a729c08a8d8435c7c5ccdedbf20e4
Trent Robbins <robbintt@gmail.com>
2017-08-18 01:06:39 -0700

    Clarified upper on repeating key xor result test.

--- set1/repeating_key_xor.py
+++ set1/repeating_key_xor.py
@@ -40,6 +40,6 @@ if __name__ == '__main__':
     result1 = base64.b16encode(repeating_key_xor(input1, repeating_keys))
 
     print result1
-    expected_result = "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f".upper()
-    print result1 == expected_result
+    expected_result = "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"
+    print result1 == expected_result.upper()
 
